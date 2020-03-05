f8cd5697b42f59a69bef5a2d0bfa3cf8c3aa5294
Xiaoyi Liu <lxy878@gmail.com>
2020-03-05 11:08:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,10 +3,10 @@ require 'pry'
 def reverse_each_word(sentence)
   words = sentence.split
   words.each do |word|
-    puts
+    puts word.reverse
   end
   # binding.pry
 end
 
 s = "Hello there, and how are you?"
-puts reverse_each_word(s)
+reverse_each_word(s)
